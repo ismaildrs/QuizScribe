af4beede4ac35113e773b7588eee35483d885378
Imad IDALI OUALI <imadidaliouali@gmail.com>
2024-11-18 13:19:25 +0100

    feat: integrate Prisma with MongoDB

--- auth.js
+++ auth.js
@@ -1,7 +1,11 @@
 import NextAuth from "next-auth";
+import { PrismaAdapter } from "@auth/prisma-adapter";
 import Google from "next-auth/providers/google";
+import { prisma } from "@/prisma";
 
-export const { handlers, signIn, signOut, auth } = NextAuth({
+export const { handlers, auth, signIn, signOut } = NextAuth({
+  adapter: PrismaAdapter(prisma),
   providers: [Google],
   pages: { signIn: "/login" },
+  debug: process.env.NODE_ENV !== "production",
 });
